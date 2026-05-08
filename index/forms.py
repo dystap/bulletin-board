a98e28035ccbf84f36bb05f74bd6e875740a1114
fiorenza <fiorenza.angelyn@gmail.com>
2026-05-08 21:43:04 +0700

    FINALLY FINISGHED THE FORMS

--- index/forms.py
+++ index/forms.py
@@ -1,6 +1,9 @@
 from django import forms
 
-from index.models import User
+from models import User
+from models import Topic
+from models import Post
+from models import Comments
 
 class UserForm(forms.Form):
     email = forms.EmailField(
@@ -66,5 +69,82 @@ class UserForm(forms.Form):
 
         User.objects.create(
             email = cleaned_data.get("email"),
+            username = cleaned_data.get("username"),
+            birthday = cleaned_data.get("birthday"),
+            hobby = cleaned_data.get("hobby"),
+            quote = cleaned_data.get("quote"),
+            pfp = cleaned_data.get("pfp"),
             
         )
+
+class TopicForm(forms.ModelForm):
+    class Meta:
+        models = Topic
+        fields = ['topic']
+        widgets =  {
+            'topic': forms.TextInput(attrs={
+                'class': 'form-control',
+                'placeholder': 'Enter Topic',
+            }),
+           
+        }
+
+    def clean(self):
+        cleaned_data = super().clean()
+
+        return cleaned_data    
+
+    def save(self):
+        cleaned_data = self.cleaned_data
+
+        Topic.objects.create(
+            topic = cleaned_data.get("topic"),
+            
+        )
+
+class PostForm(forms.ModelForm):
+    class Meta:
+        models = Post
+        fields = ['post', 'description', 'image', 'topic']
+        widget = {
+            'post': forms.TextInput(attrs={
+                'class': 'form-controls',
+                'placeholder': 'Enter Post Title',
+            }),
+            'description': forms.TextInput(attrs={
+                'class': 'form-controls',
+                'placeholder': 'Enter Post Description',
+            }),
+            'image': forms.FileInput(attrs={
+                'class': 'form-controls',
+                'placeholder': 'Put Image Here'
+            })
+        }
+
+    def clean(self):
+        cleaned_data = super().clean()
+        return cleaned_data
+    
+    def save(self):
+        cleaned_data = self.cleaned_data
+        Post.objects.create(
+            post = cleaned_data.get('post'),
+            description = cleaned_data.get('description'),
+            image = cleaned_data.get('image'),
+        )
+
+
+class CommentForm(forms.ModelForm):
+    class Meta: 
+        models = Comments
+        fields = ['post', 'comment']
+        widget = {
+            'post': forms.TextInput(attrs={
+                'class': 'form-controls',
+                'placeholder': 'Enter Comment Title',
+            }),
+            'comment': forms.TextInput(attrs={
+                'class': 'form-controls',
+                'placeholder': 'Enter Comment Content',
+            }),   
+        }
